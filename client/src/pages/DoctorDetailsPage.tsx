6d5591f7c78a3de69ace138abaf13be3e4ae589d
Maria Litwa <contact@marialitwa.com>
2024-02-12 16:09:42 +0100

    Add one-page-back navigation button on details card component.

--- client/src/pages/DoctorDetailsPage.tsx
+++ client/src/pages/DoctorDetailsPage.tsx
@@ -1,15 +1,18 @@
 // import React from 'react'
 
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Doctor } from "../@types/doctors";
 import DoctorDetails from "../components/DoctorDetails";
+import styled from "styled-components";
 
 const apiUrl = "http://localhost:5000/api/doctors";
 
 export default function DoctorDetailsPage() {
   const [doctor, setDoctor] = useState<Doctor | null>(null);
   const { id } = useParams();
+  const navigate = useNavigate();
+
   // console.log(id);
 
   async function fetchData() {
@@ -32,5 +35,27 @@ export default function DoctorDetailsPage() {
     fetchData();
   }, [id]);
 
-  return <>{doctor && <DoctorDetails doctor={doctor} />}</>;
+  return (
+    <>
+      {doctor && <DoctorDetails doctor={doctor} />}
+      <ButtonContainer>
+        <Button onClick={() => navigate(-1)}>Back</Button>
+      </ButtonContainer>
+    </>
+  );
 }
+
+// STYLING
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  margin: 2em 0;
+  width: 40%;
+  background-color: whitesmoke;
+  padding: 0.5em 0;
+  border-radius: 1em;
+`;
