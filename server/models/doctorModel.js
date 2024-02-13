bf07316fc1dd83f4da35687d5ebf1f6dc6a056bb
Maria Litwa <contact@marialitwa.com>
2024-02-13 10:41:04 +0100

    Update doctorModel.js

--- server/models/doctorModel.js
+++ server/models/doctorModel.js
@@ -12,3 +12,21 @@ const doctorSchema = new mongoose.Schema({
 const DoctorModel = mongoose.model("doctor", doctorSchema);
 
 export default DoctorModel;
+
+
+
+// OPTIONal in case I would split up address field
+
+// const doctorSchema = new mongoose.Schema({
+
+//     medical_specialty: {type: String, required: false},
+//     name: { type: String, required: true},
+//     city_district: { type: String, required: false},
+//     address: {
+//         city: { type: String,required: false},
+//         street { type: String,required: false},
+//         housenumber: { type: String,required: false},
+//         postalcode: { type: String,required: false},
+//     }
+//     phone_number: { type: String, required: false}
+// })
