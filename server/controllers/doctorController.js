e05485d5764b44e341aceb3211c1147fa5796338
Maria Litwa <contact@marialitwa.com>
2024-02-20 11:00:19 +0100

    Add deleteCard to doctor controller function.

--- server/controllers/doctorController.js
+++ server/controllers/doctorController.js
@@ -1,5 +1,7 @@
 import DoctorModel from "../models/doctorModel.js"
 
+
+// GET ALL DOCUMENTS FROM MY DB COLLECTION => complete list of all doctors
 const getAllDoctors = async(request, response) => {
 
   // sort method here to reverse order
@@ -18,6 +20,7 @@ const getAllDoctors = async(request, response) => {
     }
 }
 
+// GET SINGLE DOCUMENT FROM MY DB COLLECTION => get a specific, single doctor 
 const getDoctorById = async(request, response) => {
 
   const id = request.params.id
@@ -29,6 +32,8 @@ const getDoctorById = async(request, response) => {
   console.log(request)
 }
 
+
+// ADD A DOCTOR / DOCUMENT TO MY DB COLLECTION => add a single doctor 
 const addCard = async (request, response) => {
 
   // Start with test in Postman
@@ -53,5 +58,29 @@ const addCard = async (request, response) => {
   }
 }
 
-export { getAllDoctors, getDoctorById, addCard }
+
+
+// DELETE A DOCTOR / DOCUMENT FROM MY DB COLLECTION => delete a specific doctor 
+
+const deleteCard = async(request, response) => {
+
+  const id = request.params.id;
+
+  try {
+    const findCurrentCard = await DoctorModel.findByIdAndDelete(id)
+
+    if (!findCurrentCard) {
+      return response.status(404).json({ message: "Card not found" })
+    }
+    return response.status(200).json({ message: "Successfully deleted" })
+
+  } catch (error) {
+    console.error();
+    return response.status(500).json({ message: "Could not delete document. Please try again."})
+    
+  }
+}
+
+
+export { getAllDoctors, getDoctorById, addCard, deleteCard }
 
